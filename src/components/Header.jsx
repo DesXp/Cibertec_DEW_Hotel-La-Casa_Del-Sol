07232da93613047e3d6fb46e3a1373f938fc1a6f
DesXp <vasquezdesiderio@gmail.com>
2025-08-20 12:01:52 -0500

    Test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <header className="encabezado">
       <div className="encabezado-contenido">
-        <h1>Bienvenido a Hotel Paraíso</h1>
+        <h1>Bienvenido a Hotel Paraíso!</h1>
         <p>Tu escapada de lujo te espera</p>
         <Link to="/PaginaHabitaciones" className="btn">Ver Habitaciones</Link>
       </div>
